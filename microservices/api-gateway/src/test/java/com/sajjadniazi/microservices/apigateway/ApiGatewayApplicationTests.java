1ea3dd5527b9ed72176388559c03d49f201cade1
sajjad.niazi1 <sajjad.khan@360factors.com>
2022-01-31 16:53:12 +0500

    changed folder

---
+++ microservices/api-gateway/src/test/java/com/sajjadniazi/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajjadniazi.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

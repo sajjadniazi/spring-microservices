2ecde1053f4af9c136ca181e1e7bdd63e0ecf1aa
sajjad.niazi1 <sajjad.khan@360factors.com>
2022-02-07 19:28:52 +0500

    microservices

---
+++ microservices/currency-conversion-service/src/main/java/com/sajjadniazi/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.sajjadniazi.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(
+			@PathVariable(value = "from") String from,
+			@PathVariable(value = "to") String to);
+
+}

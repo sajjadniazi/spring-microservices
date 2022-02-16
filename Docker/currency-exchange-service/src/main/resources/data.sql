659d488d71fc88a3df49d859898d0be5be1e2c00
sajjad.niazi1 <sajjad.khan@360factors.com>
2022-02-16 11:32:03 +0500

    Docker images

---
+++ Docker/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','PKR',165,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','PKR',75,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'AUD','PKR',25,'');
